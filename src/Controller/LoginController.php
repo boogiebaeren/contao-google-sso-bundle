c0b8b6449a015e1e376122bb99072ce3bb9d7954
Felix Schnabel <f.schnabel@tum.de>
2026-09-01 02:09:56 +0200

    Fix UriSigner for Symfony 7

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -18,7 +18,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\UriSigner;
+use Symfony\Component\HttpFoundation\UriSigner;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
